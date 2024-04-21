767188ff380d20e273ced48cb548a88b9571bbc5
Salim Al Mandhari <salim.mandhari17@gmail.com>
2024-04-21 16:47:03 +0400

    add files

---
+++ Models/Alakrot models/NB classifier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 22:40:09 2019
+
+@author: salimalmandhari
+"""
+import pandas as pd  
+import numpy as np  
+import matplotlib.pyplot as plt 
+
+# import the data
+data = pd.read_csv("dataset.csv")
+
+print(data.head())
